2d3dfc41c8b4059aa554663491b000af79608408
pooo <tinkbusiness631@gmail.com>
2026-02-26 07:52:22 -0800

    adding input methode

--- BroCode/entering-data-from-html/using-prompt/script.js
+++ BroCode/entering-data-from-html/using-prompt/script.js
@@ -1,7 +1,18 @@
 let circumference;
-let radius;
-let pi = 3.14159
-
+const radiusInput = document.getElementById('input1');
+const pi = 3.14159;
+const p2 = document.getElementById('p2');
+const submit = document.getElementById('submit');
+//using prompt method 
 radius = window.prompt('enter the circle diameter');
 circumference = 2 * pi * `${radius}`;
 document.getElementById('p1').innerHTML = `your circumference value is ${circumference}`;
+
+
+// using html input method
+submit.onclick = function() {
+  const radius = Number(radiusInput.value); // get the value from input
+  circumference = 2 * pi * radius;
+  p2.textContent = circumference;
+}
+
